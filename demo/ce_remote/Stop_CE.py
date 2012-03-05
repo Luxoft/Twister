b1bdbaa9dd381ee3b0412253ff09b2c18674dcd9
Cristi Constantin <CrConstantin@luxoft.com>
2012-03-05 13:34:20 +0200

    Adapted configs for our test database.

--- demo/ce_remote/Stop_CE.py
+++ demo/ce_remote/Stop_CE.py
@@ -2,8 +2,8 @@
 
 import xmlrpclib
 
-#proxy = xmlrpclib.ServerProxy('http://11.126.32.9:8000/')	# Tsc Server
-proxy = xmlrpclib.ServerProxy('http://11.126.32.12:8000/')	# Dan Ubuntu
+proxy = xmlrpclib.ServerProxy('http://11.126.32.9:8000/')	# Tsc Server
+#proxy = xmlrpclib.ServerProxy('http://11.126.32.12:8000/')	# Dan Ubuntu
 #proxy = xmlrpclib.ServerProxy('http://11.126.32.14:8000/')	# Cro Windows
 #proxy = xmlrpclib.ServerProxy('http://10.0.2.15:8000/')	# OpenSUSE VM
 
