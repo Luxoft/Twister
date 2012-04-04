51baa7f02e3c0a32119664424cc4e9c19d0ecf7a
Cristi Constantin <CrConstantin@luxoft.com>
2012-04-04 13:42:21 +0300

    Added testbed for Resource Allocator. Updated Resource Allocator to use the testbed, pre-release. MUST TEST MORE.

--- src/server/resourceallocator/ResourceAllocatorClasses.py
+++ src/server/resourceallocator/ResourceAllocatorClasses.py
@@ -4,9 +4,9 @@ import sys
 import re
 import pickle
 
-TWISTER_PATH = os.getenv('TWISTER_PATH')
+TWISTER_PATH = r'd:\Projects\twister_rel1\src' #os.getenv('TWISTER_PATH')
 if not TWISTER_PATH:
-    print('TWISTER_PATH environment variable  is not set! Exiting!')
+    print('TWISTER_PATH environment variable is not set! Exiting!')
     exit(1)
 sys.path.append(TWISTER_PATH)
 
@@ -20,25 +20,32 @@ RESOURCE_RESERVED = 2
 lvl_1_pool = 0
 lvl_2_pool = 0
 lvl_3_pool = 0
+lvl_4_pool = 0
 
 #
-# resid format is : xx_xx_xxx , Device_Module_Port
+# resid format is : xx_xx_xx_xxx = TestBed_Device_Module_Port
 #
 
 def genidLevel(lvl):
-    global lvl_1_pool,lvl_2_pool,lvl_3_pool
-    if lvl==0:
+    global lvl_1_pool, lvl_2_pool, lvl_3_pool, lvl_4_pool
+
+    if not lvl:
         lvl_1_pool += 1
-        resid_str="%02d_00_000" % (lvl_1_pool)
-        print 'Creating Device with id: %s' % resid_str
+        resid_str="%02d_00_00_000" % (lvl_1_pool)
+        logDebug('\nCreating TestBed with id: %s' % resid_str)
     elif lvl==1:
         lvl_2_pool += 1
-        resid_str="%02d_%02d_000" % (lvl_1_pool,lvl_2_pool)
-        print 'Creating Module with id: %s' % resid_str
-    else:
+        resid_str="%02d_%02d_00_000" % (lvl_1_pool,lvl_2_pool)
+        logDebug('Creating Device with id: %s' % resid_str)
+    elif lvl==2:
         lvl_3_pool += 1
-        resid_str="%02d_%02d_%03d" % (lvl_1_pool,lvl_2_pool,lvl_3_pool)
-        print 'Creating Port with id: %s' % resid_str
+        resid_str="%02d_%02d_%02d_000" % (lvl_1_pool,lvl_2_pool,lvl_3_pool)
+        logDebug('Creating Module with id: %s' % resid_str)
+    else:
+        lvl_4_pool += 1
+        resid_str="%02d_%02d_%02d_%03d" % (lvl_1_pool,lvl_2_pool,lvl_3_pool,lvl_4_pool)
+        logDebug('Creating Port with id: %s' % resid_str)
+
     return resid_str
 
 #
@@ -47,16 +54,18 @@ class ResourceAllocator:
 
     def __init__(self, cfgfile):
         self.xmldoc = None
-        self.hwdevices = []
+        self.testbeds = []
         self.residStr = 'resid'
         self.parseConfigFile(cfgfile)
+
 
     def echo(self, msg):
         '''
         Simple echo function, for testing connection.
         '''
-        print('Echo: %s' % str(msg))
+        logDebug('Echo: %s' % str(msg))
         return 'RA reply: %s' % str(msg)
+
 
     def getPropDict(self, node, ignoretag):
         '''
@@ -72,6 +81,7 @@ class ResourceAllocator:
             propdict[key] = value
         return propdict
 
+
     def parseConfigFile(self, cfgfile):
         '''
         Parse the XML data and create lists of dictionaries to represent the devices.
@@ -83,35 +93,45 @@ class ResourceAllocator:
             logCritical('Devices config file not found:', cfgfile)
             exit(1)
 
-        global lvl_1_pool,lvl_2_pool,lvl_3_pool
+        global lvl_1_pool, lvl_2_pool, lvl_3_pool, lvl_4_pool
 
-        for dv_prop in self.xmldoc.findAll('device'):
-            # Create a dictionary from device first level xml tags, ignore devicemodule tag
-            # Generate lvl 1 resource id identifier
-            # Create device module list
-            hw_device = self.getPropDict(dv_prop, ignoretag='devicemodule')
-            hw_device[self.residStr] = genidLevel(0)
-            hw_device['status'] = RESOURCE_FREE
-            hw_device['devicemodule'] = []
+        for tb_prop in self.xmldoc.findAll('testbed'):
+            test_bed = self.getPropDict(tb_prop, ignoretag='device')
+            test_bed[self.residStr] = genidLevel(0)
+            test_bed['status'] = RESOURCE_FREE
+            test_bed['device'] = []
             lvl_2_pool = 0
 
-            # Find all devicemodule
-            for mod_prop in dv_prop.findAll('devicemodule'):
-
-                hw_device_module = self.getPropDict(mod_prop, ignoretag='deviceport')
-                hw_device_module[self.residStr] = genidLevel(1)
-                hw_device_module['status'] = RESOURCE_FREE
-                hw_device_module['deviceport'] = []
+            for dv_prop in self.xmldoc.findAll('device'):
+                # Create a dictionary from device first level xml tags, ignore devicemodule tag
+                # Generate lvl 1 resource id identifier
+                # Create device module list
+                hw_device = self.getPropDict(dv_prop, ignoretag='devicemodule')
+                hw_device[self.residStr] = genidLevel(1)
+                hw_device['status'] = RESOURCE_FREE
+                hw_device['devicemodule'] = []
                 lvl_3_pool = 0
 
-                for port_prop in mod_prop.findAll('deviceport'):
-                    hw_device_port = self.getPropDict(port_prop, ignoretag='')
-                    hw_device_port[self.residStr] = genidLevel(2)
-                    hw_device_port['status'] = RESOURCE_FREE
-                    hw_device_module['deviceport'].append(hw_device_port)
-                hw_device['devicemodule'].append(hw_device_module)
+                # Find all devicemodule
+                for mod_prop in dv_prop.findAll('devicemodule'):
 
-            self.hwdevices.append(hw_device)
+                    hw_device_module = self.getPropDict(mod_prop, ignoretag='deviceport')
+                    hw_device_module[self.residStr] = genidLevel(2)
+                    hw_device_module['status'] = RESOURCE_FREE
+                    hw_device_module['deviceport'] = []
+                    lvl_4_pool = 0
+
+                    for port_prop in mod_prop.findAll('deviceport'):
+                        hw_device_port = self.getPropDict(port_prop, ignoretag='')
+                        hw_device_port[self.residStr] = genidLevel(3)
+                        hw_device_port['status'] = RESOURCE_FREE
+
+                        hw_device_module['deviceport'].append(hw_device_port)
+                    hw_device['devicemodule'].append(hw_device_module)
+                test_bed['device'].append(hw_device)
+
+            self.testbeds.append(test_bed)
+
 
     def deviceQuery(self, query):
         '''
@@ -132,6 +152,8 @@ class ResourceAllocator:
 
         query_len = len(keyval_list)
 
+        if not query_len:
+            logError('Null query!')
 
         def checkQuery(item, query):
             # Pairs of `&&` queries
@@ -160,44 +182,55 @@ class ResourceAllocator:
             return result
 
 
-        for device in self.hwdevices:
-            if query_len>0:
-                # Devices
-                #key   = keyval_list[0][0]
-                #value = keyval_list[0][1]
-                if checkQuery(device, keyval_list[0]):
-                    if(query_len==1):
-                        return device[self.residStr]
-                    else:
-                        # Device modules
-                        for devicemodule in device['devicemodule']:
-                            #key   = keyval_list[1][0]
-                            #value = keyval_list[1][1]
-                            if checkQuery(devicemodule, keyval_list[1]):
-                                if(query_len==2):
-                                    return devicemodule[self.residStr]
-                                else:
-                                    # Device ports
-                                    #key   = keyval_list[2][0]
-                                    #value = keyval_list[2][1]
-                                    for deviceport in devicemodule['deviceport']:
-                                        if checkQuery(deviceport, keyval_list[2]):
-                                            return deviceport[self.residStr]
+        for testbed in self.testbeds:
+
+            # Testbeds
+            if checkQuery(testbed, keyval_list[0]):
+                if query_len==1:
+                    return testbed[self.residStr]
+                else:
+
+                    # Devices
+                    for device in testbed['device']:
+                        if checkQuery(device, keyval_list[1]):
+                            if query_len==2:
+                                return device[self.residStr]
+                            else:
+
+                                # Device modules
+                                for devicemodule in device['devicemodule']:
+                                    if checkQuery(devicemodule, keyval_list[2]):
+                                        if query_len==3:
+                                            return devicemodule[self.residStr]
+                                        else:
+
+                                            # Device ports
+                                            for deviceport in devicemodule['deviceport']:
+                                                if checkQuery(deviceport, keyval_list[3]):
+                                                    return deviceport[self.residStr]
 
         # If nothing is found...
         return None
+
 
     def getResourceById(self, resid):
         '''
         Returns the dictionary with this particular Res ID.
         '''
-        d = int(resid.split('_')[0]) # Device
-        m = int(resid.split('_')[1]) # Module
-        p = int(resid.split('_')[2]) # Port
-        dev = self.hwdevices[d-1]
+        t = int(resid.split('_')[0]) # Test bed
+        d = int(resid.split('_')[1]) # Device
+        m = int(resid.split('_')[2]) # Module
+        p = int(resid.split('_')[3]) # Port
+
+        tb = self.testbeds[t-1]
+        dev = None
         mod = None
         port = None
 
+        if not d:
+            return tb
+        else:
+            dev = tb['device'][d-1]
         if not m:
             return dev
         else:
@@ -208,6 +241,7 @@ class ResourceAllocator:
             port = mod['deviceport'][p-1]
         return port
 
+
     def getResourceStatus(self, resid):
         '''
         Returns the status Integer: 0, 1, or 2.
@@ -215,6 +249,7 @@ class ResourceAllocator:
         try: res = self.getResourceById(resid)
         except: print 'Cannot find Resource with Id `%s` !' % resid ; return -1
         return res.get('status', -1)
+
 
     def allocResource(self, resid, epid):
         if self.getResourceStatus(resid):
@@ -225,6 +260,7 @@ class ResourceAllocator:
         res['status'] = RESOURCE_BUSY
         return self.getResourceStatus(resid)
 
+
     def reserveResource(self, resid, epid):
         if self.getResourceStatus(resid):
             print 'Cannot allocate! The resource is already busy!'
@@ -234,50 +270,54 @@ class ResourceAllocator:
         res['status'] = RESOURCE_RESERVED
         return self.getResourceStatus(resid)
 
+
     def freeResource(self, resid):
         try: res = self.getResourceById(resid)
-        except: print 'Cannot find Resource with Id `%s` !' % resid ; return False
+        except: print 'Cannot free! Cannot find Resource with Id `%s` !' % resid ; return False
         res['status'] = RESOURCE_FREE
         return self.getResourceStatus(resid)
+
 
     def dumpConfig(self):
         '''
         Used for debug.
         '''
-        for dv in self.hwdevices:
-            print '********************'
+        for dv in self.testbeds:
+            print '\n********** Test Bed **********'
             print dv
 
 #
 
 if __name__ == '__main__':
-    #ra = ResourceAllocator('/home/dancioata/tscproject/twister/Config/hwdevices.xml')
-    ra = ResourceAllocator(r'd:\Projects\twister\Config\hwdevices.xml')
+    #ra = ResourceAllocator('/home/dancioata/tscproject/twister/Config/testbeds.xml')
+    ra = ResourceAllocator(r'd:\Projects\twister_rel1\src\config\hwconfig.xml')
     ra.dumpConfig()
 
     query_list=[
-    'devicename:DUT5||DUT6,moduletype:?',
-    'devicetype:Contivity&&devicefamily:27XX&&devicemodel:2750SY',
-    'devicevendor:Avaya&&devicetype:PBX,moduletype:?',
-    'devicevendor:Avaya,moduletype:DS32',
-    'devicename:?,moduletype:ATM',
-    'devicefamily:?,moduletype:DS32',
-    'devicefamily:ip500,moduletype:ETR||ATM||DS32,port:3',
-    'devicetype:?,moduletype:?,port:103',
-    'devicename:foo_PBX,moduletype:ETR,port:?',
-    'devicetype:PBX,moduletype:ETR,port:201',
-    'devicename:foo_PBX,moduletype:?,port:201',
-    'devicetype:?,moduletype:?,port:207',
-    'devicetype:?,moduletype:?,porttype:Phone',
-    'devicetype:?,moduletype:?,hasspeaker:True',
+    'tbname:TB-001,devicename:DUT3||DUT2||DUT1',
+    'tbname:?,devicename:DUT5||DUT6,moduletype:?',
+    'tbname:?,devicetype:Contivity&&devicefamily:27XX&&devicemodel:2750SY',
+    'tbname:?,devicevendor:Avaya&&devicetype:PBX,moduletype:?',
+    'tbname:?,devicevendor:Avaya,moduletype:DS32',
+
+    'tbid:?,devicename:?,moduletype:ATM',
+    'tbid:?,devicefamily:?,moduletype:DS32',
+    'tbid:?,devicefamily:ip500,moduletype:ETR||ATM||DS32,port:3',
+    'tbid:?,devicetype:?,moduletype:?,port:103',
+    'tbid:?,devicename:foo_PBX,moduletype:ETR,port:?',
+    'tbid:?,devicetype:PBX,moduletype:ETR,port:201',
+    'tbid:?,devicename:foo_PBX,moduletype:?,port:201',
+    'tbid:?,devicetype:?,moduletype:?,port:207',
+    'tbid:?,devicetype:?,moduletype:?,porttype:Phone',
+    'tbid:?,devicetype:?,moduletype:?,hasspeaker:True',
     ]
 
     test = True
     for query in query_list:
-        print '####################'
+        print('\n####################')
         res = ra.deviceQuery(query)
-        zero = ra.freeResource(res)
         print 'ID:', res
+        zero = ra.freeResource(res)
         print 'Alocating:', ra.allocResource(res, '')
         print 'Releasing:', ra.freeResource(res)
         if not res or zero != 0: test = False
