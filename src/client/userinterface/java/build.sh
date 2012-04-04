87aee455faf987e08c0c343991927f9112d9d2b8
Cristi Constantin <CrConstantin@luxoft.com>
2012-04-04 10:21:06 +0300

    Updated guide about how to compile the Java GUI. Updated the build scripts to work like the guide.

--- src/client/userinterface/java/build.sh
+++ src/client/userinterface/java/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-JARSIGNER=/opt/java1.7/bin/jarsigner
-JAVAC=/opt/java1.7/bin/javac
-JAR=/opt/java1.7/bin/jar
+
+JDK_PATH=/opt/java1.7/bin
+
 EXTLIBS=extlibs/jsch-0.1.44.jar:\
 extlibs/ws-commons-util-1.0.2.jar:\
 extlibs/commons-vfs-1.0.jar:\
@@ -10,10 +10,16 @@ extlibs/jcommon-1.0.16.jar:\
 extlibs/jxl.jar:\
 extlibs/ws-commons-util-1.0.2.jar:\
 extlibs/xmlrpc-client-3.1.3.jar:\
-extlibs/xmlrpc-common-3.1.3.jar\ 
+extlibs/xmlrpc-common-3.1.3.jar
 
-$JAVAC -version
-$JAVAC -d classes -source 1.6 -target 1.6 -cp $EXTLIBS src/*.java;
-cd classes;
-$JAR cf ../target/test.jar  Icons  *.class 
-$JARSIGNER ../target/test.jar Twister -storepass password
+# Compile sources
+$JDK_PATH/javac  -deprecation -d classes -source 1.6 -target 1.6  -cp $EXTLIBS  src/*.java
+
+# Generate JAR file
+$JDK_PATH/jar  cf target/applet.jar classes/Icons classes/*.class
+
+# Sign the JAR file using the keystore
+$JDK_PATH/jarsigner  target/applet.jar Twister -storepass password
+
+# Export the keystore as X509
+$JDK_PATH/keytool  -export -alias Twister -rfc -file target/sig.x509 -storepass password
