9c50c4ff1331f043ead3afd1b6857dd7b15a64a9
Daniel Cioata <dcioata@luxoft.com>
2012-05-03 11:55:01 +0300

    sign jar files

--- src/client/userinterface/java/build.sh
+++ src/client/userinterface/java/build.sh
@@ -16,10 +16,21 @@ extlibs/xmlrpc-common-3.1.3.jar
 $JDK_PATH/javac  -deprecation -d classes -source 1.6 -target 1.6  -cp $EXTLIBS  src/*.java
 
 # Generate JAR file
-$JDK_PATH/jar  cf target/applet.jar classes/Icons classes/*.class
+cd classes;
+$JDK_PATH/jar  cf ../target/applet.jar Icons *.class
 
 # Sign the JAR file using the keystore
-$JDK_PATH/jarsigner  target/applet.jar Twister -storepass password
+$JDK_PATH/jarsigner ../target/applet.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/commons-logging-1.1.1.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/commons-vfs-1.0.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/jcommon-1.0.16.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/jsch-0.1.44.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/jxl.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/VFSJFileChooser-0.0.3.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/ws-commons-util-1.0.2.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/xmlrpc-client-3.1.3.jar Twister -storepass password
+$JDK_PATH/jarsigner ../extlibs/xmlrpc-common-3.1.3.jar Twister -storepass password
+
 
 # Export the keystore as X509
 $JDK_PATH/keytool  -export -alias Twister -rfc -file target/sig.x509 -storepass password
