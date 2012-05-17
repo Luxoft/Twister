5f8669ddd93c3d74e8c349d0e4ea3640a7882126
Luxoft Twister <tsc_twister_list@itcnetworks.ro>
2012-05-17 11:43:34 +0300

    Added licence for all Python files.

--- src/client/executionprocess/TestCaseRunnerClasses.py
+++ src/client/executionprocess/TestCaseRunnerClasses.py
@@ -1,4 +1,26 @@
 
+# File: TestCaseRunnerClasses.py ; This file is part of Twister.
+
+# Copyright (C) 2012 , Luxoft
+
+# Authors:
+#    Andrei Costachi <acostachi@luxoft.com>
+#    Andrei Toma <atoma@luxoft.com>
+#    Cristian Constantin <crconstantin@luxoft.com>
+#    Daniel Cioata <dcioata@luxoft.com>
+
+# Twister is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+
+# Twister is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Twister.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 REQUIRED Python 2.7
 This file contains classes that will run TCL/ Python/ Sikuli test cases.
--- src/server/resourceallocator/ResourceAllocator.py
+++ src/server/resourceallocator/ResourceAllocator.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+# File: ResourceAllocator.py ; This file is part of Twister.
+
+# Copyright (C) 2012 , Luxoft
+
+# Authors:
+#    Andrei Costachi <acostachi@luxoft.com>
+#    Andrei Toma <atoma@luxoft.com>
+#    Cristian Constantin <crconstantin@luxoft.com>
+#    Daniel Cioata <dcioata@luxoft.com>
+
+# Twister is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+
+# Twister is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Twister.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 Resource allocator server.
 '''
