61335ab286a60c77ff9e210899fdd130e0d6530b
andreicostachi <acostachi@luxoft.com>
2012-06-07 09:51:39 +0300

    Latest Version

--- src/client/userinterface/java/src/XMLTableStructure.java
+++ src/client/userinterface/java/src/XMLTableStructure.java
@@ -14,81 +14,54 @@ import java.awt.Font;
 import java.util.ArrayList;
 import java.io.InputStream;
 
-public class XMLTableStructure {
-
-	private DocumentBuilderFactory dbf;
-	private DocumentBuilder db;
-	private Document doc;
-	private Node fstNode, secNode, trdNode;
-	private Element fstElmnt, fstNmElmnt, secElmnt, secNmElmnt, trdElmnt,
-			trdNmElmnt;
-	private NodeList fstNmElmntLst, fstNm, fstNmElmntLst2, secNmElmntLst,
-			secNm, secNmElmntLst2, trdNmElmntLst, trdNm, trdNmElmntLst2,
-			trdNm2;
-	private File f;
-	private String name, value;
-	private ArrayList<Integer> editable = new ArrayList<Integer>();
-	private ArrayList<String> columns = new ArrayList<String>();
-
-	public XMLTableStructure(InputStream in) {
-		dbf = DocumentBuilderFactory.newInstance();
-		try {
-			db = dbf.newDocumentBuilder();
-			doc = db.parse(in);
-			doc.getDocumentElement().normalize();
-			in.close();
-		} catch (ParserConfigurationException e) {
-			try {
-				in.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			System.out.println("Could not create a XML parser configuration");
-		} catch (SAXException e) {
-			try {
-				in.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			System.out.println("The document is empty or not valid");
-		} catch (IOException e) {
-			try {
-				in.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			System.out.println("Could not read the document");
-		} catch (Exception e) {
-			try {
-				in.close();
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			e.printStackTrace();
-		}
-	}
-
-	public ArrayList<String> parseXML() {
-		NodeList nodeLst = doc.getElementsByTagName("Column");
-		if (nodeLst.getLength() == 0) {
-			System.out.println("Table structure document has no Column tags");
-		}
-		for (int s = 0; s < nodeLst.getLength(); s++) {
-			fstNode = nodeLst.item(s);
-			fstElmnt = (Element) fstNode;
-			fstNmElmntLst = fstElmnt.getElementsByTagName("Name");
-			fstNmElmnt = (Element) fstNmElmntLst.item(0);
-			fstNm = fstNmElmnt.getChildNodes();
-			columns.add(fstNm.item(0).getNodeValue().toString());
-			fstNmElmntLst = fstElmnt.getElementsByTagName("Editable");
-			if (fstNmElmntLst.getLength() == 1) {
-				editable.add(columns.size() - 1);
-			}
-		}
-		return columns;
-	}
-
-	public ArrayList<Integer> getEditableColumns() {
-		return editable;
-	}
-}
+public class XMLTableStructure{
+    private DocumentBuilderFactory dbf;
+    private DocumentBuilder db;
+    private Document doc;
+    private Node fstNode,secNode,trdNode;
+    private Element fstElmnt,fstNmElmnt,secElmnt,secNmElmnt,trdElmnt,trdNmElmnt;
+    private NodeList fstNmElmntLst,fstNm,fstNmElmntLst2,secNmElmntLst,secNm,secNmElmntLst2,trdNmElmntLst,trdNm,trdNmElmntLst2,trdNm2;
+    private File f;
+    private String name,value;
+    private ArrayList<Integer > editable = new ArrayList<Integer>();
+    private ArrayList<String> columns = new ArrayList<String>();
+    
+    public XMLTableStructure (InputStream in){
+        dbf = DocumentBuilderFactory.newInstance();
+        try{db = dbf.newDocumentBuilder();
+            doc = db.parse(in);
+            doc.getDocumentElement().normalize();
+            in.close();}
+        catch(ParserConfigurationException e){
+            try{in.close();}
+            catch(Exception ex){ex.printStackTrace();}
+            System.out.println("Could not create a XML parser configuration");}
+        catch(SAXException e){
+            try{in.close();}
+            catch(Exception ex){ex.printStackTrace();}
+            System.out.println("The document is empty or not valid");}
+        catch(IOException e){
+            try{in.close();}
+            catch(Exception ex){ex.printStackTrace();}
+            System.out.println("Could not read the document");}
+        catch(Exception e){
+            try{in.close();}
+            catch(Exception ex){ex.printStackTrace();}
+            e.printStackTrace();}}
+        
+    public ArrayList<String>  parseXML(){    
+        NodeList nodeLst = doc.getElementsByTagName("Column");
+        if(nodeLst.getLength()==0)System.out.println("Table structure document has no Column tags");
+        for(int s = 0; s < nodeLst.getLength(); s++){
+            fstNode = nodeLst.item(s);
+            fstElmnt = (Element)fstNode;            
+            fstNmElmntLst = fstElmnt.getElementsByTagName("Name");
+            fstNmElmnt = (Element)fstNmElmntLst.item(0);
+            fstNm = fstNmElmnt.getChildNodes();
+            columns.add(fstNm.item(0).getNodeValue().toString());                
+            fstNmElmntLst = fstElmnt.getElementsByTagName("Editable");
+            if(fstNmElmntLst.getLength()==1) editable.add(columns.size()-1);}
+        return columns;}
+                    
+    public ArrayList<Integer> getEditableColumns(){  
+        return editable;}}
