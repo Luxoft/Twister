f72d8a6ec1b2423f2085126aeee04cad2efec998
andreicostachi <acostachi@luxoft.com>
2012-07-02 10:08:19 +0300

    latest version

--- src/client/userinterface/java/src/Device.java
+++ src/client/userinterface/java/src/Device.java
@@ -1,5 +1,5 @@
 /*
-File: Device.java ; This file is part of Twister.
+File: applet.java ; This file is part of Twister.
 
 Copyright (C) 2012 , Luxoft
 
@@ -36,194 +36,104 @@ import java.awt.Dimension;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class Device {
-
-	private int X, Y;
-	String name = "";
-	String description = "";
-	String id = "";
-	String vendor = "";
-	String type = "";
-	String family = "";
-	String model = "";
-	ArrayList<String[]> properties = new ArrayList<String[]>();
-	ArrayList<DeviceModule> modules = new ArrayList<DeviceModule>();
-	Device reference;
-
-	public Device() {
-		reference = this;
-	}
-
-	public void updateInfo() {
-		Repository.window.mainpanel.p4.getDut().additem.setEnabled(true);
-		Repository.window.mainpanel.p4.getDut().additem.setText("Add module");
-		Repository.window.mainpanel.p4.getDut().remitem.setEnabled(true);
-		Repository.window.mainpanel.p4.getDut().remitem
-				.setText("Remove device");
-		Repository.window.mainpanel.p4.getDut().temp = reference;
-		Repository.window.mainpanel.p4.getDut().tname.setText(name.toString());
-		Repository.window.mainpanel.p4.getDut().ttype.setText(type.toString());
-		Repository.window.mainpanel.p4.getDut().tvendor.setText(vendor
-				.toString());
-		Repository.window.mainpanel.p4.getDut().tmodel
-				.setText(model.toString());
-		Repository.window.mainpanel.p4.getDut().tfamily.setText(family
-				.toString());
-		Repository.window.mainpanel.p4.getDut().tid.setText(id.toString());
-		Repository.window.mainpanel.p4.getDut().tdescription
-				.setText(description.toString());
-		Repository.window.mainpanel.p4.getDut().propname.setText("");
-		Repository.window.mainpanel.p4.getDut().propvalue.setText("");
-		updatePropertys();
-	}
-
-	public void updatePropertys() {
-		Repository.window.mainpanel.p4.getDut().properties.removeAll();
-		if (Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(
-				Repository.window.mainpanel.p4.getDut().nodetemp1
-						.getChildCount() - 1).isLeaf()) {
-			while (Repository.window.mainpanel.p4.getDut().nodetemp1
-					.getChildCount() > 6) {
-				((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-						.getModel())
-						.removeNodeFromParent(((DefaultMutableTreeNode) Repository.window.mainpanel.p4
-								.getDut().nodetemp1.getChildAt(6)));
-			}
-		} else {
-			while (Repository.window.mainpanel.p4.getDut().nodetemp1
-					.getChildAt(6).isLeaf()) {
-				((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-						.getModel())
-						.removeNodeFromParent(((DefaultMutableTreeNode) Repository.window.mainpanel.p4
-								.getDut().nodetemp1.getChildAt(6)));
-			}
-		}
-		for (int i = 0; i < properties.size(); i++) {
-			DefaultMutableTreeNode child2 = new DefaultMutableTreeNode(
-					properties.get(i)[0] + " - " + properties.get(i)[1], false);
-			if (Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(
-					Repository.window.mainpanel.p4.getDut().nodetemp1
-							.getChildCount() - 1).isLeaf()) {
-				((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-						.getModel()).insertNodeInto(child2,
-						Repository.window.mainpanel.p4.getDut().nodetemp1,
-						Repository.window.mainpanel.p4.getDut().nodetemp1
-								.getChildCount());
-			} else {
-				((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-						.getModel()).insertNodeInto(child2,
-						Repository.window.mainpanel.p4.getDut().nodetemp1,
-						6 + i);
-			}
-			final JButton b = new JButton("remove");
-			b.setBounds(280, i * 23 + 18, 78, 19);
-			b.addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent ev) {
-					properties.remove(Repository.window.mainpanel.p4.getDut().properties
-							.getComponentZOrder(b) / 3);
-					updatePropertys();
-				}
-			});
-			Repository.window.mainpanel.p4.getDut().properties.add(b);
-			final JTextField text1 = new JTextField();
-			text1.setText(properties.get(i)[0]);
-			text1.setBounds(6, i * 23 + 18, 135, 25);
-			text1.addKeyListener(new KeyAdapter() {
-
-				public void keyReleased(KeyEvent ev) {
-					properties.get(Repository.window.mainpanel.p4.getDut().properties
-							.getComponentZOrder(text1) / 3)[0] = text1
-							.getText();
-					((DefaultMutableTreeNode) Repository.window.mainpanel.p4
-							.getDut().nodetemp1
-							.getChildAt(6 + (Repository.window.mainpanel.p4
-									.getDut().properties
-									.getComponentZOrder(text1) / 3))).setUserObject(text1
-							.getText()
-							+ " - "
-							+ properties.get(Repository.window.mainpanel.p4
-									.getDut().properties
-									.getComponentZOrder(text1) / 3)[1]);
-					((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-							.getModel()).nodeChanged(Repository.window.mainpanel.p4
-							.getDut().nodetemp1
-							.getChildAt(6 + (Repository.window.mainpanel.p4
-									.getDut().properties
-									.getComponentZOrder(text1) / 3)));
-				}
-			});
-			final JTextField text2 = new JTextField();
-			text2.setText(properties.get(i)[1]);
-			text2.setBounds(143, i * 23 + 18, 135, 25);
-			text2.addKeyListener(new KeyAdapter() {
-
-				public void keyReleased(KeyEvent ev) {
-					properties.get(Repository.window.mainpanel.p4.getDut().properties
-							.getComponentZOrder(text1) / 3)[1] = text2
-							.getText();
-					((DefaultMutableTreeNode) Repository.window.mainpanel.p4
-							.getDut().nodetemp1
-							.getChildAt(6 + (Repository.window.mainpanel.p4
-									.getDut().properties
-									.getComponentZOrder(text1) / 3)))
-							.setUserObject(properties
-									.get(Repository.window.mainpanel.p4
-											.getDut().properties
-											.getComponentZOrder(text1) / 3)[0]
-									+ " - " + text2.getText());
-					((DefaultTreeModel) Repository.window.mainpanel.p4.getDut().explorer.tree
-							.getModel()).nodeChanged(Repository.window.mainpanel.p4
-							.getDut().nodetemp1
-							.getChildAt(6 + (Repository.window.mainpanel.p4
-									.getDut().properties
-									.getComponentZOrder(text1) / 3)));
-				}
-			});
-			Repository.window.mainpanel.p4.getDut().properties.add(text2);
-			Repository.window.mainpanel.p4.getDut().properties.add(text1);
-		}
-		Repository.window.mainpanel.p4.getDut().properties
-				.setPreferredSize(new Dimension(Repository.window.mainpanel.p4
-						.getDut().properties.getWidth(),
-						properties.size() * 23 + 18));
-		Repository.window.mainpanel.p4.getDut().properties.revalidate();
-		Repository.window.mainpanel.p4.getDut().properties.repaint();
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void setID(String id) {
-		this.id = id;
-	}
-
-	public void setVendor(String vendor) {
-		this.vendor = vendor;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String toString() {
-		return "Device: " + name.toString();
-	}
-
-	public void addModule(DeviceModule module) {
-		modules.add(module);
-	}
-
-	public void setFamily(String family) {
-		this.family = family;
-	}
-
-	public void setModel(String model) {
-		this.model = model;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-}
+public class Device{
+    private int X,Y;
+    String name= "";
+    String description="";
+    String id="";
+    String vendor="";
+    String type="";
+    String family="";
+    String model="";
+    ArrayList <String[]> properties =  new ArrayList <String[]>();
+    ArrayList <DeviceModule> modules =  new ArrayList <DeviceModule>();
+    Device reference;
+    
+    public Device(){reference = this;}
+        
+    public void updateInfo(){
+        Repository.window.mainpanel.p4.getDut().additem.setEnabled(true);
+        Repository.window.mainpanel.p4.getDut().additem.setText("Add module");
+        Repository.window.mainpanel.p4.getDut().remitem.setEnabled(true);
+        Repository.window.mainpanel.p4.getDut().remitem.setText("Remove device");
+        Repository.window.mainpanel.p4.getDut().temp = reference;
+        Repository.window.mainpanel.p4.getDut().tname.setText(name.toString());
+        Repository.window.mainpanel.p4.getDut().ttype.setText(type.toString());
+        Repository.window.mainpanel.p4.getDut().tvendor.setText(vendor.toString());
+        Repository.window.mainpanel.p4.getDut().tmodel.setText(model.toString());
+        Repository.window.mainpanel.p4.getDut().tfamily.setText(family.toString());
+        Repository.window.mainpanel.p4.getDut().tid.setText(id.toString());        
+        Repository.window.mainpanel.p4.getDut().tdescription.setText(description.toString());
+        Repository.window.mainpanel.p4.getDut().propname.setText("");
+        Repository.window.mainpanel.p4.getDut().propvalue.setText("");
+        updatePropertys();}
+        
+    public void updatePropertys(){
+        Repository.window.mainpanel.p4.getDut().properties.removeAll();
+        if(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildCount()-1).isLeaf()){
+            while(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildCount()>6){
+                ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).removeNodeFromParent(((DefaultMutableTreeNode)Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6)));}}
+        else{
+            while(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6).isLeaf()){
+                ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).removeNodeFromParent(((DefaultMutableTreeNode)Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6)));}}
+        for(int i =0;i<properties.size();i++){
+            DefaultMutableTreeNode child2 = new DefaultMutableTreeNode(properties.get(i)[0]+" - "+properties.get(i)[1],false);
+            if(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildCount()-1).isLeaf()){
+                ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).insertNodeInto(child2,Repository.window.mainpanel.p4.getDut().nodetemp1,Repository.window.mainpanel.p4.getDut().nodetemp1.getChildCount());}
+            else{
+                ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).insertNodeInto(child2,Repository.window.mainpanel.p4.getDut().nodetemp1,6+i);}
+            final JButton b = new JButton("remove");
+            b.setBounds(280,i*23+18,78,19);
+            b.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ev){
+                    properties.remove(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(b)/3);
+                    updatePropertys();}});
+            Repository.window.mainpanel.p4.getDut().properties.add(b);
+            final JTextField text1 = new JTextField();
+            text1.setText(properties.get(i)[0]);
+            text1.setBounds(6,i*23+18,135,25);
+            text1.addKeyListener(new KeyAdapter(){
+                public void keyReleased(KeyEvent ev){
+                    properties.get(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)[0]=text1.getText();
+                    ((DefaultMutableTreeNode)Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6+(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3))).setUserObject(text1.getText()+" - "+properties.get(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)[1]);
+                    ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).nodeChanged(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6+(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)));}});
+            final JTextField text2 = new JTextField();
+            text2.setText(properties.get(i)[1]);
+            text2.setBounds(143,i*23+18,135,25);    
+            text2.addKeyListener(new KeyAdapter(){
+                public void keyReleased(KeyEvent ev){
+                    properties.get(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)[1]=text2.getText();
+                    ((DefaultMutableTreeNode)Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6+(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3))).setUserObject(properties.get(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)[0]+" - "+text2.getText());
+                    ((DefaultTreeModel)Repository.window.mainpanel.p4.getDut().explorer.tree.getModel()).nodeChanged(Repository.window.mainpanel.p4.getDut().nodetemp1.getChildAt(6+(Repository.window.mainpanel.p4.getDut().properties.getComponentZOrder(text1)/3)));}});
+            Repository.window.mainpanel.p4.getDut().properties.add(text2);
+            Repository.window.mainpanel.p4.getDut().properties.add(text1);}
+        Repository.window.mainpanel.p4.getDut().properties.setPreferredSize(new Dimension(Repository.window.mainpanel.p4.getDut().properties.getWidth(),properties.size()*23+18));
+        Repository.window.mainpanel.p4.getDut().properties.revalidate();
+        Repository.window.mainpanel.p4.getDut().properties.repaint();}
+        
+    public void setDescription(String description){
+        this.description = description;}
+        
+    public void setID(String id){
+        this.id=id;}
+        
+    public void setVendor(String vendor){
+        this.vendor = vendor;}
+        
+    public void setType(String type){
+        this.type=type;}
+        
+    public String toString(){
+        return "Device: "+name.toString();}
+        
+    public void addModule(DeviceModule module){
+        modules.add(module);}
+        
+    public void setFamily(String family){
+        this.family=family;}
+    
+    public void setModel(String model){
+        this.model=model;}
+            
+    public void setName(String name){
+        this.name=name;}}
