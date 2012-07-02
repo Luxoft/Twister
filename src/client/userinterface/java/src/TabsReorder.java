e290de4e0366a6cf3504d2584618c16121e5fe88
andreicostachi <acostachi@luxoft.com>
2012-07-02 10:14:34 +0300

    latest version

--- src/client/userinterface/java/src/TabsReorder.java
+++ src/client/userinterface/java/src/TabsReorder.java
@@ -1,5 +1,5 @@
 /*
-File: TabsReorder.java ; This file is part of Twister.
+File: applet.java ; This file is part of Twister.
 
 Copyright (C) 2012 , Luxoft
 
@@ -20,43 +20,28 @@ import javax.swing.event.MouseInputAdapter;
 import javax.swing.JTabbedPane;
 import java.awt.event.MouseEvent;
 
-public class TabsReorder extends MouseInputAdapter {
+public class TabsReorder extends MouseInputAdapter{
+    private JTabbedPane tabbed;
+    private int tab;
+    
+    protected TabsReorder(JTabbedPane pane){
+        this.tabbed = pane;
+        tab = -1;}
 
-	private JTabbedPane tabbed;
-	private int tab;
+    public static void enableReordering(JTabbedPane pane){
+        TabsReorder instance = new TabsReorder(pane);
+        pane.addMouseListener(instance);
+        pane.addMouseMotionListener(instance);}
+    
+    public void mouseReleased(MouseEvent ev){tab = -1;}
+        
+    public void mousePressed(MouseEvent ev) {tab = tabbed.getUI().tabForCoordinate(tabbed,ev.getX(), ev.getY());}
 
-	protected TabsReorder(JTabbedPane pane) {
-		this.tabbed = pane;
-		tab = -1;
-	}
-
-	public static void enableReordering(JTabbedPane pane) {
-		TabsReorder instance = new TabsReorder(pane);
-		pane.addMouseListener(instance);
-		pane.addMouseMotionListener(instance);
-	}
-
-	public void mouseReleased(MouseEvent ev) {
-		tab = -1;
-	}
-
-	public void mousePressed(MouseEvent ev) {
-		tab = tabbed.getUI().tabForCoordinate(tabbed, ev.getX(), ev.getY());
-	}
-
-	public void mouseDragged(MouseEvent ev) {
-		if (tab == -1) {
-			return;
-		}
-		int targetTabIndex = tabbed.getUI().tabForCoordinate(tabbed, ev.getX(),
-				ev.getY());
-		if (targetTabIndex != -1 && targetTabIndex != tab) {
-			boolean isForwardDrag = targetTabIndex > tab;
-			tabbed.insertTab(tabbed.getTitleAt(tab), tabbed.getIconAt(tab),
-					tabbed.getComponentAt(tab), tabbed.getToolTipTextAt(tab),
-					isForwardDrag ? targetTabIndex + 1 : targetTabIndex);
-			tab = targetTabIndex;
-			tabbed.setSelectedIndex(tab);
-		}
-	}
-}
+    public void mouseDragged(MouseEvent ev){
+        if(tab == -1)return;
+        int targetTabIndex = tabbed.getUI().tabForCoordinate(tabbed, ev.getX(), ev.getY());
+        if(targetTabIndex != -1 && targetTabIndex != tab){
+            boolean isForwardDrag = targetTabIndex > tab;
+            tabbed.insertTab(tabbed.getTitleAt(tab), tabbed.getIconAt(tab), tabbed.getComponentAt(tab), tabbed.getToolTipTextAt(tab), isForwardDrag ? targetTabIndex+1 : targetTabIndex);
+            tab = targetTabIndex;
+			tabbed.setSelectedIndex(tab);}}}
