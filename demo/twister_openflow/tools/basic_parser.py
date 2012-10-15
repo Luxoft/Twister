79349b46c0b057cf448e2b485f64d76f72736fd1
Cristi Constantin <CrConstantin@Luxoft.com>
2012-10-15 13:36:19 +0300

    The runner downloads libs in the folder of each EP, instead of a global cache folder. Renamed some of the internal exposed variables. Added Twister_Openflow demo folder.

---
+++ demo/twister_openflow/tools/basic_parser.py
@@ -0,0 +1,26 @@
+
+from oftest_parser import *
+
+template="""
+from ce_libs import *
+
+try:
+    basic.basic_config=getOpenflowConfig(globEpName)
+    basic.basic_port_map=basic.basic_config[\'port_map\']
+except:
+    print \"Error: Invalid configuration for EPNAME: \" + str(globEpName)
+
+%s
+
+tc = %s()
+_RESULT = tc.run()
+"""
+
+config={
+"tc_template":template,
+"tc_suite":"../oflib/basic.py",
+"tc_list_name":"basic_list",
+"tc_ignore_list":["DataPlaneOnly"],
+"tc_path":"../tests/"
+}
+parse_test_suite(config)
