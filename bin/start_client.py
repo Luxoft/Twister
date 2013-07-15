1046cda7d98b388c6b6a5a0631a6d8a03559a7bd
bogdan <bpopescu@luxoft.com>
2013-07-15 09:51:27 +0300

    Fix for start_client to avoid socket binding to localhost

--- bin/start_client.py
+++ bin/start_client.py
@@ -1,7 +1,29 @@
 #!/usr/bin/python
 
-# version: 1.000
+# File: start_client.py ; This file is part of Twister.
 
+# version: 2.001
+
+# Copyright (C) 2012-2013 , Luxoft
+
+# Authors:
+#    Adrian Toader <adtoader@luxoft.com>
+#    Andrei Costachi <acostachi@luxoft.com>
+#    Andrei Toma <atoma@luxoft.com>
+#    Cristi Constantin <crconstantin@luxoft.com>
+#    Daniel Cioata <dcioata@luxoft.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # This file will register ALL Execution Processes that are enabled,
 # from file `twister/config/epname.ini` !
 # To be able to start the packet sniffer, this must run as ROOT.
@@ -144,7 +166,7 @@ class TwisterClientService():
 		serverEstablished = False
 		while not serverEstablished or not self.clientPort > 4488:
 			try:
-				self.server = SimpleXMLRPCServer(("localhost", self.clientPort),
+				self.server = SimpleXMLRPCServer(("0.0.0.0", self.clientPort),
 													requestHandler=ServiceHandler)
 				self.server.register_introspection_functions()
 
