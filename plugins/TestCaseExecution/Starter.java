b835605e73299b69eaa7c1cc16934e876a7e8338
Costachi Andrei <acostachi@itcnetworks.ro>
2013-08-02 15:02:16 +0300

    changed hardcoded string

--- plugins/TestCaseExecution/Starter.java
+++ plugins/TestCaseExecution/Starter.java
@@ -1,3 +1,22 @@
+/*
+File: Starter.java ; This file is part of Twister.
+Version: 2.001
+
+Copyright (C) 2012-2013 , Luxoft
+
+Authors: Andrei Costachi <acostachi@luxoft.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import com.twister.plugin.twisterinterface.TwisterPluginInterface;
 import com.twister.plugin.twisterinterface.CommonInterface;
 import com.twister.plugin.baseplugin.BasePlugin;
@@ -23,7 +42,7 @@ public class Starter implements TwisterPluginInterface{
                 container.removeAll();
                 container.revalidate();
                 container.repaint();
-                RunnerRepository.initialize("true","tsc-server",container);
+                RunnerRepository.initialize("true",variables.get("host"),container);
                 try{
                     container.getAppletContext().showDocument(new URL("javascript:resize()"));
                 } catch (Exception e) {
