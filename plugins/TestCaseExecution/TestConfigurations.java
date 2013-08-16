c475abc20d076d44e81cab81fc613b98b6911cc9
Costachi Andrei <acostachi@itcnetworks.ro>
2013-08-16 15:23:08 +0300

    added classes

---
+++ plugins/TestCaseExecution/TestConfigurations.java
@@ -0,0 +1,51 @@
+/*
+File: TestConfigurations.java ; This file is part of Twister.
+Version: 2.004
+
+Copyright (C) 2012-2013 , Luxoft
+
+Authors: Andrei Costachi <acostachi@luxoft.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.SwingUtilities;
+import java.awt.Dimension;
+
+public class TestConfigurations extends JPanel{
+    private JPanel editor;
+    private ConfigTree tree;
+    private ConfigEditor cfgedit;
+    
+
+    public TestConfigurations(){
+        setLayout(new BorderLayout());
+        cfgedit = new ConfigEditor();
+        tree = new ConfigTree();
+        final JSplitPane sp = new JSplitPane();
+        sp.setLeftComponent(tree);
+        sp.setRightComponent(cfgedit);
+        sp.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+        sp.setResizeWeight(0.5);
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    sp.setDividerLocation(0.5);
+                }
+            });
+        add(sp,BorderLayout.CENTER);
+        tree.setConfigEditor(cfgedit);
+    }
+
+}
