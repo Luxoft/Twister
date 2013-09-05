8ff8f22d79bf85d365e183a144f3a732fce4c967
Cristi Constantin <CrConstantin@Luxoft.com>
2013-09-05 14:08:18 +0300

    Implemented user checking for users and groups, on NIS domains. Added a few tests to check the statuses, for the applet.

---
+++ demo/test-statuses/init_pass.py
@@ -0,0 +1,27 @@
+
+#
+# version: 2.001
+# <title>init file</title>
+# <description>Test status</description>
+#
+
+# `USER`, `EP`, `SUITE_NAME` and `FILE_NAME` are magic variables,
+# injected inside all Twister tests.
+
+import os
+import sys
+import time
+
+print 'I am init file.'
+print 'I am not doing anything special, just printing some variables.'
+
+print 'Hello, user', USER, '!'
+print 'System Under Test:', SUT
+print 'Exec process:', EP
+print 'Suite:', SUITE_NAME
+print 'Remote file:', FILE_NAME
+print 'Local file:', __file__
+
+_RESULT = 'PASS'
+
+#
