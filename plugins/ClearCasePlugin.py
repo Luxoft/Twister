4b196ce87a963937d55ca7c933036dc81723472e
Cristi Constantin <CrConstantin@Luxoft.com>
2013-09-23 17:31:43 +0300

    Clearcase Plugin fix, to run per user. After you are done with it, send the command "exit".

--- plugins/ClearCasePlugin.py
+++ plugins/ClearCasePlugin.py
@@ -1,86 +1,125 @@
-#!/usr/bin/env python
-#
-# -*- coding: utf-8 -*-
-#
-# version: 1.000
-#
-# File: PacketSnifferPlugin.py ; This file is part of Twister.
-#
-# Copyright (C) 2012 , Luxoft
-#
-#
+
+# File: ClearCasePlugin.py ; This file is part of Twister.
+
+# version: 2.002
+
+# Copyright (C) 2012-2013 , Luxoft
+
 # Authors:
 #    Adrian Toader <adtoader@luxoft.com>
-#
-#
+#    Andrei Costachi <acostachi@luxoft.com>
+#    Andrei Toma <atoma@luxoft.com>
+#    Cristi Constantin <crconstantin@luxoft.com>
+#    Daniel Cioata <dcioata@luxoft.com>
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
-#
+
 # http://www.apache.org/licenses/LICENSE-2.0
-#
+
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-
 
 """
     ClearCase plugin wraps cleartool library.
     Executes the given commands using cleartool library.
 """
-
+from __future__ import print_function
 
 from BasePlugin import BasePlugin
 
-from json import dumps as jsonDump
+import os, sys
+import time
+import random
+import socket
+import subprocess
+import xmlrpclib
 
+TWISTER_PATH = os.getenv('TWISTER_PATH')
+if not TWISTER_PATH:
+    print('TWISTER_PATH environment variable is not set! Exiting!')
+    exit(1)
 
-try:
-    import cleartool
-except Exception as e:
-    raise e
+from common.helpers import userHome
 
-
-
+#
 
 class Plugin(BasePlugin):
-    """ ClearCase plugin. """
+
+    """
+    ClearCase plugin
+    """
 
     def __init__(self, user, data):
-        if not data:
-            return False
 
+        if not data: return False
         BasePlugin.__init__(self, user, data)
-
         self.user = user
         self.data = data
+        self.conn = None
+
+
+    def _connect(self):
+
+        # Searching for a free port in the safe range...
+        while 1:
+            free = False
+            port = random.randrange(59000, 60000)
+            try:
+                socket.create_connection((None, port), 1)
+            except:
+                free = True
+            if free: break
+
+        p_cmd = 'su {} -c "{} -u {}/plugins/ClearCaseSrv.py {}"'.format(self.user, sys.executable, TWISTER_PATH, port)
+        proc = subprocess.Popen(p_cmd, cwd='{}/twister'.format(userHome(self.user)), shell=True)
+        proc.poll()
+
+        time.sleep(0.5)
+        print('CC Srv for user `{}` launched on `127.0.0.1:{}` - PID `{}`.'.format(self.user, port, proc.pid))
+
+        try:
+            self.conn = xmlrpclib.ServerProxy('http://127.0.0.1:{}/'.format(port))
+            self.conn.hello()
+        except Exception as e:
+            print('Cannot connect to CC Srv on `127.0.0.1:{}` - `{}` !'.format(port, e))
+            proc.terminate()
+            self.conn = None
+            return False
+
+        return True
 
 
     def run(self, args):
-        """  """
+        """
+        Called for every command
+        """
+        # Create connection ...
+        if not isinstance(self.conn, xmlrpclib.ServerProxy):
+            resp = self._connect()
+            # Connection failed !
+            if not resp:
+                return False
+        try:
+            self.conn.hello()
+        except:
+            resp = self._connect()
+            # Connection failed !
+            if not resp:
+                return False
+        try:
+            resp = self.conn.cmd(args)
+        except Exception as e:
+            print('CC Srv: Exception on args `{}` - `{}` !'.format(args, e))
+            return False
 
-        args = {k: v[0] if isinstance(v, list) else v for k,v in args.iteritems()}
+        return resp
 
-        # response structure
-        response = False
-
-        if not args.has_key('command') or not isinstance(args['command'], str):
-            return response
-
-        _response = cleartool.cmd(args['command'])
-        response = {
-            'status': _response[0],
-            'data': _response[1],
-            'error': _response[2]
-        }
-
-        return jsonDump(response)
-
-
-
+#
 
 """
 
@@ -94,4 +133,3 @@ class Plugin(BasePlugin):
 </Plugin>
 
 """
-
---
+++ plugins/ClearCaseSrv.py
@@ -0,0 +1,121 @@
+
+# File: ClearCaseSrv.py ; This file is part of Twister.
+
+# version: 2.001
+
+# Copyright (C) 2012-2013 , Luxoft
+
+# Authors:
+#    Adrian Toader <adtoader@luxoft.com>
+#    Andrei Costachi <acostachi@luxoft.com>
+#    Andrei Toma <atoma@luxoft.com>
+#    Cristi Constantin <crconstantin@luxoft.com>
+#    Daniel Cioata <dcioata@luxoft.com>
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import os
+import sys
+
+import cherrypy
+from cherrypy import _cptools
+
+try: import simplejson as json
+except: import json
+
+TWISTER_PATH = os.getenv('TWISTER_PATH')
+if not TWISTER_PATH:
+    print('TWISTER_PATH environment variable is not set! Exiting!')
+    exit(1)
+if TWISTER_PATH not in sys.path:
+    sys.path.append(TWISTER_PATH)
+
+try:
+    import cleartool
+except Exception as e:
+    print('Cannot import ClearTool !'.format(e))
+
+#
+
+class CC(_cptools.XMLRPCController):
+
+
+    @cherrypy.expose
+    def hello(self):
+        """
+        Hello function, for testing connection.
+        """
+        return True
+
+
+    @cherrypy.expose
+    def cmd(self, args):
+        """
+        Send cleartool command.
+        setview ... endview ... ls ...
+        """
+        if not isinstance(args, dict):
+            return False
+
+        args = {k: v[0] if isinstance(v, list) else v for k, v in args.iteritems()}
+
+        if not args.has_key('command') or not isinstance(args['command'], str):
+            return False
+
+        if args['command'] == 'exit':
+            cherrypy.engine.exit()
+
+        _r = cleartool.cmd(args['command'])
+
+        response = {
+            'status': _r[0],
+            'data':   _r[1],
+            'error':  _r[2],
+        }
+
+        return json.dumps(response)
+
+#
+
+if __name__ == "__main__":
+
+    serverPort = sys.argv[1:2]
+
+    if not serverPort:
+        print('Clearcase Plugin Srv: Must start with parameter PORT number!')
+        exit(1)
+    else:
+        try:
+            serverPort = int(serverPort[0])
+        except:
+            print('Clearcase Plugin Srv: Must start with parameter PORT number!')
+            exit(1)
+
+    # Config
+    conf = {'global': {
+            'server.socket_host': '127.0.0.1',
+            'server.socket_port': serverPort,
+            'server.thread_pool': 2,
+            'engine.autoreload.on': False,
+            'log.screen': False
+            }
+        }
+
+    print('Starting Clearcase Plugin Srv on `{}`.'.format(serverPort))
+
+    # Start !
+    cherrypy.quickstart(CC(), '/', config=conf)
+
+    print('Ending Clearcase Plugin Srv from `{}`.'.format(serverPort))
