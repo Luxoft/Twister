df5ec74049f5335c4135a98b4e6183a377915c04
Costachi Andrei <acostachi@itcnetworks.ro>
2013-10-11 14:14:49 +0300

    ClearCase initialization on changestate removed Buttons from ClearCase About info resize recreation of plugins.xml in case it has size 0

--- client/userinterface/java/src/MainRepository.java
+++ client/userinterface/java/src/MainRepository.java
@@ -1,6 +1,6 @@
 /*
 File: MainRepository.java ; This file is part of Twister.
-Version: 2.019
+Version: 2.020
 
 Copyright (C) 2012-2013 , Luxoft
 
@@ -72,8 +72,8 @@ public class MainRepository {
     private static TwisterPluginInterface plugin;
     private static XmlRpcClient client;
     private static Hashtable<String,String> hash = new Hashtable<String,String>();
-    private static String version = "2.037";
-    private static String builddate = "10.10.2013";
+    private static String version = "2.038";
+    private static String builddate = "11.10.2013";
     public static int time = 10;//seconds
     public static boolean countdown = false;
     public static String logotxt;
