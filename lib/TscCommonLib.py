61193f2b30129552528d4aed84ece929fabd118a
Cristi Constantin <CrConstantin@Luxoft.com>
2014-08-13 13:28:38 +0300

    Re-written users_and_groups and libs tests to use the new test_fail and assert exceptions.

--- lib/TscCommonLib.py
+++ lib/TscCommonLib.py
@@ -60,7 +60,7 @@ class TwisterException(Warning):
     def __init__(self, value=''):
         self.value = value
     def __str__(self):
-        return repr(self.value)
+        return str(self.value)
 
 
 class ExceptionTestFail(TwisterException):
